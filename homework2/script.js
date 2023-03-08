bcfdb30a3d12b2557af4a2821aa868bc017d183f
Vika <stigizimvika@ukr.net>
2023-03-08 13:28:14 +0200

    changed homework2

--- homework2/script.js
+++ homework2/script.js
@@ -1,38 +1,35 @@
 let firstNumber = prompt ('Введіть число N');
-//Ціле/не ціле
-let n = Number.isInteger(+firstNumber);
-if(!Number.isInteger(+firstNumber)){
-    alert('Помилка. Ви ввели не ціле число.')
+
+while (+firstNumber){
+if (!Number.isInteger(+firstNumber)){
+    alert ('Помилка. Ви ввели не ціле число.')
+} break
 }
 //Парне/не парне
 let evenN = Boolean(+firstNumber%2 === 0);
 
 console.log(+firstNumber)
-console.log('Число N ціле?' + ' - ' + n)
 console.log('Число N парне?' + ' - ' + evenN)
 
-
 let secondNumber = prompt ('Введіть число M');
-//Ціле/не ціле
-let m = Number.isInteger(+secondNumber);
-if(!Number.isInteger(+secondNumber)){
-    alert('Помилка. Ви ввели не ціле число.')
-}
+
+while (+secondNumber){
+    if (!Number.isInteger(+secondNumber)){
+        alert ('Помилка. Ви ввели не ціле число.')
+    } break
+    }
 //Парне/не парне
 let evenM = Boolean(+secondNumber%2 === 0);
-
 console.log(+secondNumber)
-console.log('Число M ціле?' + ' - ' + m)
 console.log('Число M парне?' + ' - ' + evenM)
-
 
 let sum = Number(firstNumber)+Number(secondNumber);
 let result = 0;
-for (let i=0; i<=sum; i+=2){
+for (let i=2; i<=sum; i+=2){
     result +=i;
-    console.log(i)
+    console.log('парні числа ' + i)
 }
-    console.log(result)
+    console.log('сума парних чисел становить ' + result)
 if (!Number.isInteger(sum)){
     alert ('Помилка. Одне з чисел було не ціле.')
 } else{
