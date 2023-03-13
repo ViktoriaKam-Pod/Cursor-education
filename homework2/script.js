95c28800b91fdef6e4a65cfc6791072758e4e77f
Vika <stigizimvika@ukr.net>
2023-03-13 16:17:26 +0200

    delete boolean in homework2

--- homework2/script.js
+++ homework2/script.js
@@ -2,35 +2,27 @@ let firstNumber;
 
 while (!Number.isInteger(+firstNumber)||+firstNumber<=0){
     firstNumber = prompt ("Введіть число N")
-    
 } 
-
-//Парне/не парне
-let evenN = Boolean(+firstNumber%2 === 0);
-
 console.log(+firstNumber)
-console.log('Число N парне?' + ' - ' + evenN)
 
 let secondNumber;
-
 while (!Number.isInteger(+secondNumber)||+firstNumber>+secondNumber){
     secondNumber = prompt ('Введіть число M (число має бути більше N)');
     }
-//Парне/не парне
-let evenM = Boolean(+secondNumber%2 === 0);
 console.log(+secondNumber)
-console.log('Число M парне?' + ' - ' + evenM)
 
 let sum1 = confirm('Пропускати парні числа?')
-if (sum1 == true){
+document.writeln(`Пропускати парні числа : ${Boolean(sum1)} <br\>`)
+if (sum1){
     sum1 = Number(firstNumber)+Number(secondNumber);
     let result = 0;
     for (let i=1; i<=sum1; i+=2){
         result +=i;
-        console.log('парні числа ' + i)
+        console.log('числа, які рахуємо ' + i)
     }
     alert ('Сума чисел становить ' + result)
     document.writeln(`Сума чисел становить : ${result} <br\>`)
+    console.log('Результат : ' + result)
 } else {
     sum1 = Number(firstNumber)+Number(secondNumber);
     let result = 0;
@@ -40,8 +32,10 @@ if (sum1 == true){
     }
     alert ('Сума чисел становить ' + result)
     document.writeln(`Сума чисел становить : ${result} <br\>`)
+    console.log('Результат : ' + result)
 } 
-document.writeln(`Пропускати парні числа : ${Boolean(sum1)} <br\>
+
+document.writeln(`
 Введено число N : ${+firstNumber} <br\>
 Введено число M : ${+secondNumber} <br\>
 `)
